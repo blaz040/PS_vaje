745ea935713ea75fa1b82ceea22de59fd010874b
blaz040 <blaz.pirc04@gmail.com>
2025-11-26 13:54:15 +0100

    upd

--- main/main.go
+++ main/main.go
@@ -1,22 +1,26 @@
 package main
 
+import (
+	redovalnica "github.com/blaz040/PS_vaje/pckg/redovalnica"
+)
+
 func main() {
 
-	var studenti map[string]Student
-	studenti = make(map[string]Student)
+	var studenti map[string]redovalnica.Student
+	studenti = make(map[string]redovalnica.Student)
 
-	studenti["1"] = Student{ime: "Jože", priimek: "Pločnik", ocene: []int{9, 9, 9}}
-	studenti["2"] = Student{"Blaž", "Pirc", []int{10, 10, 10}}
-	studenti["3"] = Student{"Maj", "Zorko", []int{8, 7, 6}}
+	studenti["1"] = redovalnica.Student{"Jože", "Pločnik", []int{9, 9, 9}}
+	studenti["2"] = redovalnica.Student{"Blaž", "Pirc", []int{10, 10, 10}}
+	studenti["3"] = redovalnica.Student{"Maj", "Zorko", []int{8, 7, 6}}
 
-	DodajOceno(studenti, "2", 6)
-	DodajOceno(studenti, "1", 5)
-	DodajOceno(studenti, "1", 11)
+	redovalnica.DodajOceno(studenti, "2", 6)
+	redovalnica.DodajOceno(studenti, "1", 5)
+	redovalnica.DodajOceno(studenti, "1", 11)
 
 	//fmt.Printf("%s povprečje: %0.2f\n", studenti["2"].ime, povprecje(studenti, "2"))
 	//fmt.Printf("%s povprečje: %0.2f\n", studenti["1"].ime, povprecje(studenti, "1"))
 	//fmt.Printf("Neko povprečje: %0.2f\n", povprecje(studenti, "4"))
 
-	IzpisRedovalnice(studenti)
-	IzpisiKoncniUspeh(studenti)
+	redovalnica.IzpisRedovalnice(studenti)
+	redovalnica.IzpisiKoncniUspeh(studenti)
 }
--- pckg/redovalnica/radovalnica.go
+++ pckg/redovalnica/radovalnica.go
@@ -3,9 +3,9 @@ package redovalnica
 import "fmt"
 
 type Student struct {
-	ime     string
-	priimek string
-	ocene   []int
+	Ime     string
+	Priimek string
+	Ocene   []int
 }
 
 func DodajOceno(studenti map[string]Student, vpisnaStevilka string, ocena int) {
@@ -65,25 +65,4 @@ func IzpisiKoncniUspeh(studenti map[string]Student) {
 		}
 		fmt.Printf("%s %s: povprečna ocena %0.1f -> %s\n", student.ime, student.priimek, povp_ocena, uspeh)
 	}
-}
-
-func main() {
-
-	var studenti map[string]Student
-	studenti = make(map[string]Student)
-
-	studenti["1"] = Student{ime: "Jože", priimek: "Pločnik", ocene: []int{9, 9, 9}}
-	studenti["2"] = Student{"Blaž", "Pirc", []int{10, 10, 10}}
-	studenti["3"] = Student{"Maj", "Zorko", []int{8, 7, 6}}
-
-	dodajOceno(studenti, "2", 6)
-	dodajOceno(studenti, "1", 5)
-	dodajOceno(studenti, "1", 11)
-
-	fmt.Printf("%s povprečje: %0.2f\n", studenti["2"].ime, povprecje(studenti, "2"))
-	fmt.Printf("%s povprečje: %0.2f\n", studenti["1"].ime, povprecje(studenti, "1"))
-	fmt.Printf("Neko povprečje: %0.2f\n", povprecje(studenti, "4"))
-
-	izpisRedovalnice(studenti)
-	izpisiKoncniUspeh(studenti)
 }
